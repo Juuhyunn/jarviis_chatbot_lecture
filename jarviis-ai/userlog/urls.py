fa70ea54e523887563275ac5420e0a11414572fd
Juuhyunn <ahnju96@gmail.com>
2021-11-18 14:13:46 +0900

    history - 18, November

--- jarviis-ai/userlog/urls.py
+++ jarviis-ai/userlog/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url
+from django.conf.urls import url, re_path
+from django.urls import path, re_path
 
 from userlog import views
 
@@ -6,5 +7,8 @@ urlpatterns = [
     url(r'process', views.process),
     url(r'upload', views.upload),
     url(r'test', views.test),
-
+    url(r'modify', views.modify),
+    url(r'remove/(?P<pk>\w{0,500})$', views.remove),
+    url(r'create', views.create),
+    url(r'list/(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})$', views.list_by_date),
 ]
--- jarviis-ai/userlog/views.py
+++ jarviis-ai/userlog/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -10,9 +12,11 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view, parser_classes
 
+from location.models_process import Location
 from userlog.models import UserLog
 from userlog.models_data import DbUploader
 from userlog.models_process import LogData
+from userlog.serializers import UserLogSerializer
 
 
 @api_view(['GET'])
@@ -35,25 +39,74 @@ def test(request):
     LogData().dummy_from_db()
     return JsonResponse({'test': 'SUCCESS'})
 
-# ===== react =====
+# ===== react api =====
 
 
-@api_view(['GET', 'POST'])
+@api_view(['PUT'])
 @parser_classes([JSONParser])
 def modify(request):
-    return JsonResponse({'getlatlng': 'SUCCESS'})
+    ic("********** modify **********")
+    edit = request.data
+    ic(edit)
+    log = UserLog.objects.get(pk=edit['id'])
+    db = UserLog.objects.all().filter(id=edit['id']).values()[0]
+    # print(f' 변경 전 : {db}')
+    db['location'] = edit['location']
+    x, y, address = Location.getAddress(edit['location'])
+    db['address'] = address
+    db['x'] = x
+    db['y'] = y
+    db['log_date'] = edit['log_date']
+    db['weather'] = edit['weather']
+    db['log_type'] = edit['log_type']
+    db['contents'] = edit['contents']
+    db['item'] = edit['item']
+    # print(f' 변경 후 : {db}')
+    serializer = UserLogSerializer(data=db)
+    # print(f'db type : {type(db)}  // serializer type : {type(serializer)}')
+    if serializer.is_valid():
+        serializer.update(log, db)
+        return JsonResponse(data=serializer.data, safe=False)
+    return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'POST'])
+@api_view(['DELETE'])
 @parser_classes([JSONParser])
-def remove(request):
-    return JsonResponse({'getlatlng': 'SUCCESS'})
+def remove(request, pk):
+    ic("********** remove **********")
+    ic(f'pk : {pk}')
+    db = UserLog.objects.get(pk=pk)
+    db.delete()
+    return JsonResponse({'User Log': 'DELETE SUCCESS'})
 
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 @parser_classes([JSONParser])
 def create(request):
-    return JsonResponse({'getlatlng': 'SUCCESS'})
+    ic("********** create **********")
+    new = request.data
+    ic(f'new : {new}')
+    if new['address'] == '':
+        x, y, address = Location().getAddress(new['location'])
+        new['address'] = address
+        new['x'] = x
+        new['y'] = y
+    else:
+        print(new['address'])
+        x, y = Location().getLatLng(addr=new['address'])
+        new['x'] = x
+        new['y'] = y
+    UserLog.objects.create(location=new['location'],
+                           address=new['address'],
+                           x=new['x'],
+                           y=new['y'],
+                           weather=new['weather'],
+                           log_type=new['log_type'],
+                           contents=new['contents'],
+                           item=new['item'],
+                           user_id=new['user_id'])
+    return JsonResponse({'USER LOG': 'CREATE SUCCESS'})
+
 
 
 @api_view(['GET', 'POST'])
@@ -64,8 +117,10 @@ def find(request):
 
 @api_view(['GET', 'POST'])
 @parser_classes([JSONParser])
-def list(request):
-    ic("********** list **********")
-    userlog = UserLog.objects.all()
-
-    return JsonResponse({'getlatlng': 'SUCCESS'})
+def list_by_date(request, year, month, day):
+    ic("********** list by date **********")
+    ic(f'date : {year}-{month}-{day}')
+    userlog = UserLog.objects.filter(log_date__year= year, log_date__month=month, log_date__day=day)
+    serializer = UserLogSerializer(userlog, many=True)
+    ic(serializer.data)
+    return JsonResponse(data = serializer.data, safe=False)
